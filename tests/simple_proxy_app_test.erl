13ac6e2d59656836836fae0f587e980374bb2d7c
cola1129 <cola1129@gmail.com>
2015-01-23 11:17:43 +0800

    add unittest

---
+++ tests/simple_proxy_app_test.erl
@@ -0,0 +1,12 @@
+%%%-------------------------------------------------------------------
+%%% @author yhwu
+%%% @copyright (C) 2014, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 15. Dec 2014 3:22 PM
+%%%-------------------------------------------------------------------
+-module(simple_proxy_app_test).
+-author("yhwu").
+
+-include_lib("eunit/include/eunit.hrl").
---
+++ tests/simple_proxy_sup_test.erl
@@ -0,0 +1,12 @@
+%%%-------------------------------------------------------------------
+%%% @author yhwu
+%%% @copyright (C) 2014, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 15. Dec 2014 4:01 PM
+%%%-------------------------------------------------------------------
+-module(simple_proxy_sup_test).
+-author("yhwu").
+
+-include_lib("eunit/include/eunit.hrl").
